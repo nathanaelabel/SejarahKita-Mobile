8c100baabff7d280f22ba8d5da1f1077bb35355b
Maumau <yujioshinozaki@gmail.com>
2021-12-05 16:16:13 +0800

    Added another url

--- app/src/main/java/com/uc/sejarahkita_mobile/helper/Const.java
+++ app/src/main/java/com/uc/sejarahkita_mobile/helper/Const.java
@@ -1,5 +1,7 @@
 package com.uc.sejarahkita_mobile.helper;
 
 public class Const {
-    public final static String BASE_URL = "http://192.168.0.8/SejarahKita-Web/public/api/";
+//    public final static String BASE_URL = "http://192.168.0.8/SejarahKita-Web/public/api/";
+
+    public final static String BASE_URL = "http://192.168.1.8/SejarahKita-Web/public/api/";
 }
